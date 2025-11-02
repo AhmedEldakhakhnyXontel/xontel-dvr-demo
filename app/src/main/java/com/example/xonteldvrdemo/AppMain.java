c5b5c8724f1aeeab4247584e0e763774a3f984da
ahmed <ahmed.eldakhakhny@xontel.com>
2025-11-02 14:55:24 +0200

    refactor: getting log data

--- app/src/main/java/com/example/xonteldvrdemo/AppMain.java
+++ app/src/main/java/com/example/xonteldvrdemo/AppMain.java
@@ -3,6 +3,7 @@ package com.example.xonteldvrdemo;
 import android.app.Application;
 import android.os.Handler;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.Player.Core.PlayerClient;
 import com.Player.Core.PlayerCore;
@@ -33,29 +34,30 @@ public class AppMain extends Application {
 		// 设置免登陆支持报警，如果服务器不支持，必须 设置ClientCore.isSuportLocalAlarmPush=false；
 		ClientCore.isSuportLocalAlarmPush = false; // 默认是不支持
 		playerclient = new PlayerClient();
-//		if (!isRun) {
-//			isRun = true;
-//			new Thread() {
-//
-//				@Override
-//				public void run() {
-//
-//					while (isRun) {
-//						String log = pc.CLTLogData(100);
-//						if (!TextUtils.isEmpty(log)) {
-//							Log.d("WriteLogThread", log);
-//						}
-//					}
-//
-//				}
-//
-//			}.start();
-//		}
-
-
-
+		getLogData();
 
 		super.onCreate();
+	}
+
+	/** this function gets log data  **/
+	private void getLogData() {
+		if (!isRun) {
+			isRun = true;
+			new Thread() {
+
+				@Override
+				public void run() {
+
+					while (isRun) {
+						String log = playerclient.CLTGetLogData(100);
+						if (!TextUtils.isEmpty(log)) {
+							Log.d("WriteLogThread ", log);
+						}
+					}
+				}
+
+			}.start();
+		}
 	}
 
 	public synchronized PlayerClient getPlayerclient() {
